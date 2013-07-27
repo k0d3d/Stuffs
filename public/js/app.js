3d31c4359b33fd7b3b1dd7b93e8cca917c37554a
k0d3d <k0d3d@localhost.node.lo>
2013-07-27 03:11:42 +0100

    Looking good with Modular Refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,22 @@
 // Declare app level module which depends on filters, and services
 
-var app = angular.module('argetni', ['ui.bootstrap']);
-app.config(function ($routeProvider, $locationProvider) {
+angular.module('integraApp', [
+  'ui.bootstrap',
+  'order',
+  'stock',
+  'item',
+  'dashboard'
+  ]);
+angular.module('integraApp').config(function ($routeProvider, $locationProvider) {
   $routeProvider.
-    when('/', {
-      templateUrl: '/home/index',
-      controller: 'indexController'
-    }).
-    when('/items/list', {
-      controller: 'itemsController',
-      templateUrl: '/items/index'
-    }).
-    when('/items/add', {
-      controller: 'itemsController',
-      templateUrl: '/items/new'
-    }).
-    when('/dashboard/order',{
-      controller: 'OrdersController',
-      templateUrl: '/orders/add'
-    }).
-    when('/orders',{
-      controller: 'OrdersController',
-      templateUrl: '/orders/all'
-    }).
     otherwise({
       redirectTo: '/'
     });
   $locationProvider.html5Mode(true);
+});
+
+angular.module('integraApp').controller('MainController', function($scope, $http){
+    $scope.arhref = function(arhrefVal){
+      window.location.href=arhrefVal;
+    }; 
 });
